52f9557ea718e783e86ff5f901b729e009917634
Rubi <rubi_garcia1994@hotmail.com>
2017-02-28 12:51:26 -0600

    prueba2

--- src/ConvolutionKernel.java
+++ src/ConvolutionKernel.java
@@ -10,77 +10,92 @@ import java.io.IOException;
 
 public class ConvolutionKernel {
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException {
 
-        int ancho; //ancho de la matriz
-        int alto; //alto de la matriz
+        //blur-si
+        /*double [][] matrix = { { 0.0625, 0.125, 0.0625},
+                { 0.125,  0.25, 0.125 },
+                { 0.0625, 0.125, 0.0625 } };*/
 
-        // edge Detection--outline
-      int [][] matrix = { { -1, -1, -1 },
+        // edge Detection--outline SI
+      double [][] matrix = { { -1, -1, -1 },
             { -1,  8, -1 },
             { -1, -1, -1 } };
 
-       //Sharpen
-       /* int [][] matrix = { { 0, -1, 0 },
+       //Sharpen -SI
+        /*double [][] matrix = { { 0, -1, 0 },
                 { -1, 5, -1 },
                 { 0, -1, 0 } };*/
 
-        //custom
-        /*int [][] matrix = { { 0, 0, 0 },
-                { 0,  1, 0 },
-                { 0, 0, 0} };
-*/
+        //Emboss SI
+        /*double [][] matrix = { { -1, -1, 0 },
+                { -1,  0, 1 },
+                { 0, 1, 1} };*/
+
         File f = null;
-        BufferedImage img=null;
+        BufferedImage imgInput=null;
+        BufferedImage imgOutput=null;
 
         try {
-            f = new File("C:\\Users\\Rubi\\Documents\\bosque.jpg");
-            img = ImageIO.read(f);
+            f = new File("C:\\Users\\Rubi\\Documents\\alemania.jpg");
+            imgInput = ImageIO.read(f);
         }catch (IOException ioe){System.out.println("No se encuentra la imagen");}
 
-        ancho=img.getWidth();//ancho de la matriz
-        alto=img.getHeight();//alto de la matriz
+        int ancho=imgInput.getWidth();//ancho de la matriz
+        int alto=imgInput.getHeight();//alto de la matriz
 
-        int p=0, a=0, r=0, g=0, b=0, suma=0;
+        imgOutput = new BufferedImage(ancho, alto, imgInput.getType());
 
-        int tamañoMatrix=3;
+        double suma=0;
+        int mult_factor = 1;
+        int tamañoMatrix = 3;
         int s = tamañoMatrix / 2;
 
-        for (int i=s; i<alto-s; i++){
-            for (int j=s; j<ancho-s; j++){
+        for (int x=0; x<ancho; x++){
+
+            for (int y=0; y<alto; y++){
 
                 int cont = 0; //acumulador a cero
+                int red=0, green=0, blue=0;
 
-                for (int k=0; k<tamañoMatrix; k++){
-                    for (int l=0; l<tamañoMatrix; l++ ){
+                for (int i=0; i<tamañoMatrix; i++){
+                    for (int j=0; j<tamañoMatrix; j++ ){
 
-                        p= img.getRGB(j,i);
+                        //Cálculo de las coordenadas X y Y del píxel a
+                        // multiplicar con el elemento actual del núcleo
+                        int imageX = (x - s + i + ancho) % ancho;
+                        int imageY = (y - s + j + alto) % alto;
 
-                        a = (matrix[k][l] * (p>>24)&0xff);
-                        r = (matrix[k][l] * (p>>16)&0xff);
-                        g = (matrix[k][l] * (p>>8)&0xff);
-                        b = (matrix[k][l] *  p&0xff);
+                       int  p = imgInput.getRGB(imageX, imageY);
+                        int r = (p>>16)&0xff;
+                        int g = (p>>8)&0xff;
+                        int b = p&0xff;
+
+                //El RGB se multiplica con el elemento actual del kernel y
+                        // se añade a las variables rojo, verde y azul
+                        red += (r*matrix[i][j]);
+                        green += (g*matrix[i][j]);
+                        blue += (b*matrix[i][j]);
 
                         cont ++;
 
-                    }//for l
+                    }//for i
+                } //for j
 
-                } //for k
+                //para asegurarse de que RGB esté dentro del alcance de 255
+                red =  Math.min(Math.max((red*mult_factor),0),255);
+				green = Math.min(Math.max((green*mult_factor),0),255);
+				blue = Math.min(Math.max((blue*mult_factor),0),255);
 
-                //a=a/cont; r=r/cont; g=g/cont; b=b/cont;
-                //suma=suma/cont;
-
-                suma = (a+r+b+g)/3;
-                p = (suma<<24)|(suma<<16)|(suma<<8)|(suma);
-
-                img.setRGB(j,i,p);
+                // Pixel se escribe en la imagen
+                imgOutput.setRGB(x,y, new Color(red, green, blue).getRGB());
 
             } //for j
         }//for i
 
         try{
-            f= new File("C:\\Users\\Rubi\\Documents\\bosqueKernel1.jpg");
-            ImageIO.write(img, "jpg", f);
+            f= new File("C:\\Users\\Rubi\\Documents\\alemaniaK.jpg");
+            ImageIO.write(imgOutput, "jpg", f);
 
         }catch (IOException ioe){System.out.println("No se encuentra la imagen");}
 
